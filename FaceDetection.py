7ca440f4edfa6dc3b509fec4cedcb2e671af7ffb
Davann Tet <davanntet@gmail.com>
2023-12-07 16:07:55 +0700

    singleton

--- FaceDetection.py
+++ FaceDetection.py
@@ -1,10 +1,16 @@
 from Data_Preprocessing import ImagePreprocessing
 from keras.models import load_model
 import numpy as np
+
 class FaceDetection:
+    _shared_state = {}
+
     def __init__(self):
-        self.model = load_model('model.h5')
-        self.image_preprocessing = ImagePreprocessing()
+        self.__dict__ = self._shared_state
+        if not self._shared_state:
+            self.model = load_model('model.h5')
+            self.image_preprocessing = ImagePreprocessing()
+
     def predict(self,image):
         img = self.image_preprocessing.facial_detection_img(image)
         if img is None:
@@ -13,4 +19,4 @@ class FaceDetection:
         result = np.array(emotion)
         maxidx = np.argmax(result)
         label = ['Angry', 'Happy', 'Nuetral'] 
-        return label[maxidx]  
+        return label[maxidx]
