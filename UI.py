8a9bd8a0d3bd26e1d618ca9dba1cd0bfb38ae10e
Visal-Som30 <visal030303@gmail.com>
2023-12-08 01:40:59 +0700

    final update

--- UI.py
+++ UI.py
@@ -1,18 +1,21 @@
 import streamlit as st
 from PIL import Image
-import pandas as pd
+import io
 import emoji
+from FaceDetection import FaceDetection
+import numpy as np
+import base64
+
+fd = FaceDetection()
 
 def instuction():
     with st.sidebar:
         st.header("📑 Instruction")
         st.markdown("1. Drag and Drop your photo into the right form. 👉")
         st.markdown("2. Just Click on the DETECT button, it will tell the emotion from your photo.")
-        # Display a hint with an emoji using st.write
-        st.write("ℹ️ **Hint:** This is a simple example with Streamlit and emojis.")
-
+  
         # Display another hint with a different emoji using st.markdown
-        st.markdown(f"{emoji.emojize(':bulb:')} **Tip:** Customize your Streamlit app with emojis!")
+        st.markdown(f"{emoji.emojize(':bulb:')} **Note:** Angry, Neutral, and Happy Emotions Only.")
 
 def main():
     st.set_page_config(
@@ -25,7 +28,7 @@ def main():
     st.title("😃 Emotion Detection 😔")
 
     # Upload image through Streamlit's file uploader
-    uploaded_file = st.file_uploader("Please Input Your Image Here to Detect Your Emotion 👇", type=["jpg", "jpeg", "png"])
+    uploaded_file = st.file_uploader("Please Input Your Image Here to Detect Your Emotion 👇", type=["jpg", "jpeg", "png"])        
     
     # Initialize session state
     if "detect_button" not in st.session_state:
@@ -36,13 +39,42 @@ def main():
     
     # Check if the button has not been clicked before
     if detect_button and not st.session_state.detect_button:
-        st.write("You are upset 😔 right now! Maybe your crush rejected your confession.")
+
+        # Read the contents of the file
+        image_bytes = uploaded_file.read()
+
+        # Use PIL to open the image from bytes
+        image = np.array(Image.open(uploaded_file))
+        # st.write(image)
+        # print(image)
+        result = fd.predict(image)
+        if result == "Happy":
+            st.write(f"You are {result} 😊 right now! Maybe your crush accepted your confession.")
+        elif result == "Angry":
+            st.write(f"You are {result} 😡 right now! Maybe your crush is dating with another guy.")
+        elif result == "Neutral":
+            st.write(f"You are {result} 😐 right now! Maybe your crush is considering your confession.")
+        else:
+            st.write("🙏 Sorry, we cannot detect your photo. Please Kindly upload another photo. 🙏")
         # Set button_clicked to True to indicate that the button has been clicked
         st.session_state.dislike_button = True
 
     if uploaded_file is not None:
-        # Display the uploaded image
-        st.image(uploaded_file, caption="Uploaded Image", use_column_width=True,)
+
+        # image_bytes = uploaded_file.read()
+
+        # # Use PIL to open the image from bytes
+        # image = Image.open(io.BytesIO(image_bytes))
+
+        # # Display the image with a smaller size
+        # st.image(image, width=500) 
+        image_bytes = uploaded_file.getvalue()
+
+        # Display the uploaded image in the center using markdown
+        st.markdown(
+            f'<div style="display: flex; justify-content: center;"><img src="data:image/png;base64,{base64.b64encode(image_bytes).decode()}",width=100px></div>',
+            unsafe_allow_html=True,
+        )
 
       
     instuction()
